d89bbe523a79b260b2f1e7640b96cd918d2f9a65
Justin Riddell <J.S.Riddell@warwick.ac.uk>
2016-12-14 10:16:52 +0000

    Added trig for better than AABB, modding size of ang by 180 degs

--- collision.cpp
+++ collision.cpp
@@ -22,7 +22,30 @@ class Cuboid {
         glm::vec3 ang_; // x, y, z
         glm::vec3 size_; // h, w, d
         glm::vec3 hsize_; // h, w, d * 1/2
+        
+        bool static areColliding(
+                const glm::vec3& c1Pos,
+                const glm::vec3& c2Pos,
+                const glm::vec3& c1HSize,
+                const glm::vec3& c2HSize
+                ) {
+            return 
+                (c1Pos.x - c1HSize.x <= c2Pos.x + c2HSize.x) &&
+                (c1Pos.x + c1HSize.x >= c2Pos.x - c2HSize.x) &&
+                (c1Pos.y - c1HSize.y <= c2Pos.y + c2HSize.y) &&
+                (c1Pos.y + c1HSize.y >= c2Pos.y - c2HSize.y) &&
+                (c1Pos.z - c1HSize.z <= c2Pos.z + c2HSize.z) &&
+                (c1Pos.z + c1HSize.z >= c2Pos.z - c2HSize.z);
+        }
 
+        glm::vec3 static angledSize(const glm::vec3& hsize, const glm::vec3& ang) {
+            // want abs as these are abs lengths
+            return glm::vec3(
+                    fabs(cos(ang.x)) * hsize.x,
+                    fabs(cos(ang.y)) * hsize.y,
+                    fabs(cos(ang.z)) * hsize.z
+                    );
+        }
 
     public:
         Cuboid(
@@ -31,17 +54,11 @@ class Cuboid {
         }
 
         bool static colliding(const Cuboid& c1, const Cuboid& c2) {
-            glm::vec3 c1Pos = c1.pos();
-            glm::vec3 c2Pos = c2.pos();
-            glm::vec3 c1HSize = c1.hsize();
-            glm::vec3 c2HSize = c2.hsize();
-            return 
-                (c1Pos.x - c1HSize.x <= c2Pos.x + c2HSize.x) &&
-                (c1Pos.x + c1HSize.x >= c2Pos.x - c2HSize.x) &&
-                (c1Pos.y - c1HSize.y <= c2Pos.y + c2HSize.y) &&
-                (c1Pos.y + c1HSize.y >= c2Pos.y - c2HSize.y) &&
-                (c1Pos.z - c1HSize.z <= c2Pos.z + c2HSize.z) &&
-                (c1Pos.z + c1HSize.z >= c2Pos.z - c2HSize.z);
+                const glm::vec3 c1Pos = c1.pos();
+                const glm::vec3 c2Pos = c2.pos();
+                const glm::vec3 c1HSize = Cuboid::angledSize(c1.hsize(),c1.ang());
+                const glm::vec3 c2HSize = Cuboid::angledSize(c2.hsize(),c2.ang());
+                return areColliding(c1Pos,c2Pos,c1HSize,c2HSize);
         }
 
         glm::vec3 pos() const {
@@ -60,13 +77,21 @@ class Cuboid {
             return hsize_;
         }
 
-        void rotate(glm::vec3 by) {
-            ang_ += by;
+        void rotateDegs(float x, float y, float z) {
+            rotateRads(
+                    (x*M_PI)/180.0f,
+                    (y*M_PI)/180.0f,
+                    (z*M_PI)/180.0f
+                );
         }
-        void rotate(float x, float y, float z) {
+
+        void rotateRads(float x, float y, float z) {
             ang_.x += x;
             ang_.y += y;
             ang_.z += z;
+            ang_.x = fmod(ang_.x,M_PI/2.0f);
+            ang_.y = fmod(ang_.y,M_PI/2.0f);
+            ang_.z = fmod(ang_.z,M_PI/2.0f);
         }
 
         void translate(glm::vec3 by) {
@@ -95,6 +120,11 @@ int main() {
     Cuboid c1 = Cuboid(1.0f,1.0f,4.0f);
     Cuboid c2 = Cuboid(1.0f,1.0f,4.0f);
     std::cout << Cuboid::colliding(c1,c2) << "\n";
-    c2.translate(1.1f,0.0f,0.0f);
-    std::cout << Cuboid::colliding(c1,c2) << "\n";
+    c2.translate(1.0f,0.0f,0.0f);
+    c2.rotateDegs(135.0f,0.0f,0.0f);
+    std::cout << "COOEEE " << c2.ang().x << "\n";
+    std::cout << (Cuboid::colliding(c1,c2) ? "They are colliding" : "They are NOT colliding") << "\n";
+    std::cout << "c1" << c1 << "\n";
+    std::cout << "c2" << c2 << "\n";
+    std::cout << "c2 x effective len " << fabs(cos(c2.ang().x))*c2.hsize().x << " full " << c2.hsize().x << "\n";
 }
