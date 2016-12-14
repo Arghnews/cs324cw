0c56beff1b3f88cb48d90c6f23297aaa35e1994f
Justin Riddell <J.S.Riddell@warwick.ac.uk>
2016-12-14 16:30:59 +0000

    Changed vertices to vec still works

--- arm.cpp
+++ arm.cpp
@@ -18,7 +18,7 @@ long timeNowSeconds();
 GLuint shaderProgram;
 GLuint VAO, VBO, EBO;
 
-GLfloat vertices[] = {
+std::vector<GLfloat> vertices = {
     0.5f,  0.5f, 0.0f,  // Top Right
     0.5f, -0.5f, 0.0f,  // Bottom Right
     -0.5f, -0.5f, 0.0f,  // Bottom Left
@@ -65,10 +65,10 @@ int main(int argc, char* argv[]) {
     // repeat this segment for each VAO/VBO binding etc
     glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), 
-            vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(GLfloat), 
+            vertices.data(), GL_STATIC_DRAW);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size()*sizeof(float), 
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size()*sizeof(GLfloat), 
             indices.data(), GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 
             3 * sizeof(GLfloat), (GLvoid*)0);
