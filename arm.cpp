eb4af781d59656fa0c8cd7a4950330bc05139e26
Justin Riddell <J.S.Riddell@warwick.ac.uk>
2016-12-14 16:26:07 +0000

    Got uniform working, spent ages before this trying to get it to work with color attrib bound also in the vertex array, simply cannot get that to work for some reason even literally copying his example code? Nothing displays, strange.

--- arm.cpp
+++ arm.cpp
@@ -5,42 +5,85 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <chrono>
+#include <math.h>
+#include <thread>
 
 std::string fileToString(std::string);
 int init(int argc, char* argv[]);
 GLuint shaders();
+long timeNowSeconds();
+
+GLuint shaderProgram;
+GLuint VAO, VBO, EBO;
+
+GLfloat vertices[] = {
+    0.5f,  0.5f, 0.0f,  // Top Right
+    0.5f, -0.5f, 0.0f,  // Bottom Right
+    -0.5f, -0.5f, 0.0f,  // Bottom Left
+    -0.5f,  0.5f, 0.0f,   // Top Left 
+    1.0f,0.9f,0.0f, //toptop left
+    -1.0f,0.9f,0.0f,//top left down a bit
+    -0.5,-1.0f,0.0f
+};
+std::vector<GLuint> indices = {  // Note that we start from 0!
+    //0, 1, 3,   // First Triangle
+    //1, 2, 3,    // Second Triangle
+    5, 4, 6    //
+};  
 
 void display() {
+    while (true) {
+        GLfloat timeValue = ((float)((timeNowSeconds() * 12) % 180) * M_PI) / 180.0f;
+        std::cout << timeValue << "\n";
+        GLfloat greenValue = sin(timeValue);
+        GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUseProgram(shaderProgram);
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
+        glBindVertexArray(VAO);
+        //someOpenGLFunctionThatDrawsOurTriangle();   
+        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+        glutSwapBuffers(); 
+    }
 }
 
 int main(int argc, char* argv[]) {
     int success = init(argc, argv);
     if (success != 0) {
-        return sucess;
+        return success;
     }
 
-    GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
-    };  
+    shaderProgram = shaders();
 
-	GLuint VBO;
-	// generates buffer object, with id 1
+    glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	// defines type of object/target of buffer calls
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// Type of buffer to copy to
-	// How GPU should manage data - freq of data changing
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    GLuint shaderProgram = shaders();
-
-
+	glGenBuffers(1, &EBO);
     
-    std::cout << "Hi from arm" << "\n";
-	glutMainLoop(); 
+    // repeat this segment for each VAO/VBO binding etc
+    glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), 
+            vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size()*sizeof(float), 
+            indices.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 
+            3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    // Uncommenting this call will result in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    std::cout << "Hi from arm " << "\n";
+    glutMainLoop(); 
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 	return 0; 
 }
 
@@ -78,7 +121,8 @@ GLuint shaders() {
 	GLuint vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
-	const char *vertShaderSource = fileToString("vertex.shader").c_str();
+    auto tempString = fileToString("vertex.shader");
+	const char *vertShaderSource = tempString.c_str();
 	// c_str() returns ptr to c-string representation of string's value
 
 	// attach shader source code to shader obj and compile shader
@@ -96,7 +140,8 @@ GLuint shaders() {
         std::cout << "Failure, vertex shader compile - \n" << infoLog << "\n";
 	}
     
-	const char *fragShaderSource = fileToString("fragment.shader").c_str();
+    auto tempString2 = fileToString("fragment.shader");
+	const char *fragShaderSource = tempString2.c_str();
 	GLuint fragmentShader;
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragShaderSource, NULL);
@@ -120,4 +165,8 @@ GLuint shaders() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
     return shaderProgram;
+}
+
+long timeNowSeconds() {
+    return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 }
